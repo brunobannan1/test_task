6a26c80c2530e93709901703c693335a854a714e
nikolay-gerasimov <31804148+nikolay-gerasimov@users.noreply.github.com>
2019-06-26 19:44:30 +0300

    finally xml???

--- src/main/java/org/nick/app/Main.java
+++ src/main/java/org/nick/app/Main.java
@@ -1,22 +1,38 @@
 package org.nick.app;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
-import java.io.File;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import org.nick.model.*;
 
 public class Main {
     static Forms forms = null;
     public static void main(String[] args) {
-        String filePath="test.xml";
-        File file = new File(filePath);
+        String filePath="example.xml";
         try {
+            XMLInputFactory xif = XMLInputFactory.newInstance();
+            XMLStreamReader xsr = xif.createXMLStreamReader(new FileReader(filePath));
+            xsr.nextTag();
+            while(xsr.hasNext()) {
+                if(xsr.isStartElement() && xsr.getLocalName().equals("forms")) {
+                    break;
+                }
+                xsr.next();
+            }
+
             JAXBContext jaxbContext = JAXBContext.newInstance(Forms.class);
             Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-            Forms forms1 = (Forms) jaxbUnmarshaller.unmarshal(file);
-            forms = forms1;
-        } catch (JAXBException e) {
+            System.out.println(xsr.getNamespaceContext().getNamespaceURI("ns"));
+            JAXBElement<Forms> forms1 = jaxbUnmarshaller.unmarshal(xsr, Forms.class);
+            System.out.println(forms1.getName());
+            forms = forms1.getValue();
+        } catch (JAXBException | FileNotFoundException | XMLStreamException e) {
             e.printStackTrace();
         }
         System.out.println(forms.toString());
